60ded682afc7a0c53c3ae853846404344d3247d1
indika <indeka9@yahoo.com>
2018-11-20 13:56:32 +0530

    test class git commit

---
+++ myChess/testClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * testClass.cpp
+ *
+ *  Created on: Nov 20, 2018
+ *      Author: indika
+ */
+
+#include "testClass.h"
+
+testClass::testClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+testClass::~testClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ myChess/testClass.h
@@ -0,0 +1,17 @@
+/*
+ * testClass.h
+ *
+ *  Created on: Nov 20, 2018
+ *      Author: indika
+ */
+
+#ifndef TESTCLASS_H_
+#define TESTCLASS_H_
+
+class testClass {
+public:
+	testClass();
+	virtual ~testClass();
+};
+
+#endif /* TESTCLASS_H_ */
